fd9a5d3f9cf59fc01a7d785b2f29f0257c7d9737
ArijitXfx <thearijitxfx@gmail.com>
2018-08-19 19:14:58 +0530

    100%

---
+++ MicroAndArrayUpdate.java
@@ -0,0 +1,114 @@
+/*
+ * Micro purchased an array A having N integer values. After playing it for a while, he got bored of it and decided to update value of its element. In one second he can increase value of each array element by 1. He wants each array element's value to become greater than or equal to K. Please help Micro to find out the minimum amount of time it will take, for him to do so.
+
+Input: 
+First line consists of a single integer, T, denoting the number of test cases. 
+First line of each test case consists of two space separated integers denoting N and K. 
+Second line of each test case consists of N space separated integers denoting the array A.
+
+Output:
+For each test case, print the minimum time in which all array elements will become greater than or equal to K. Print a new line after each test case.
+
+Constraints: 
+ 
+ 
+
+SAMPLE INPUT 
+2
+3 4
+1 2 5
+3 2
+2 5 5
+SAMPLE OUTPUT 
+3
+0
+Explanation
+For first test case,
+After 1 second, array will be 
+After 2 second, array will be 
+After 3 second, array will be 
+
+So it will take 3 second for all array elements to become greater than or equal to 4.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ 
+public class MicroAndArrayUpdate
+{
+    static class FastReader
+    {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(new
+                     InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements())
+            {
+                try
+                {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException  e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try
+            {
+                str = br.readLine();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        FastReader sc=new FastReader();
+        int test = sc.nextInt();
+        while(test-->0){
+        	int n = sc.nextInt();
+        	int k = sc.nextInt();
+        	int min = Integer.MAX_VALUE;
+        	for(int i=0;i<n;i++){
+        		int b = sc.nextInt();
+        		min = min>b?b:min;
+        	}
+        	System.out.println(k>min?k-min:0);
+        }
+    }
+}
